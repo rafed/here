2952efeabb6d226b6cd7aa2c68dce318cde482c4
Rafed <rafed123@yahoo.com>
2018-09-14 19:13:02 +0600

    Added documentation

--- here.py
+++ here.py
@@ -4,28 +4,39 @@ import json
 appID = "LqJ21lDTyLEeGYbu4t6K"
 appCode = "JaJrcnY2K-ocT9MxyhzHqA"
 
+doc = (
+    "http://traffic.api.here.com/traffic/6.0/xsd/flow.xsd"
+    "?app_id=" + appID +
+    "&app_code=" + appCode
+)
+
 weather =   (
-            "https://weather.api.here.com/weather/1.0/report.json"
-            "?app_id=" + appID +
-            "&app_code=" + appCode +
-            "&product=observation"
-            "&name=Dhaka"
-            )   
+    "https://weather.api.here.com/weather/1.0/report.json"
+    "?app_id=" + appID +
+    "&app_code=" + appCode +
+    "&product=observation"
+    "&name=Dhaka"
+)   
 
 location =  (
-            "https://traffic.api.here.com/traffic/6.2/flow.json"
-            "?app_id=" + appID + 
-            "&app_code=" + appCode +
-            "&bbox=28.624347,77.205863;28.637060,77.335174"
-            "&responseattributes=sh,fc"
-            )
+    "https://traffic.api.here.com/traffic/6.2/flow.json"
+    "?app_id=" + appID + 
+    "&app_code=" + appCode +
+    "&bbox=28.624347,77.205863;28.637060,77.335174"
+    # "&responseattributes=sh,fc"
+)
 
-#           "&bbox=28.469295,76.967812;28.672817,77.431641"
+# "&bbox=28.469295,76.967812;28.672817,77.431641"
 
-r = requests.get(location)
+r = requests.get(doc)
+# r = requests.get(location)
 # r = requests.get(weather)
 
-print r.status_code
+if r.status_code != 200:
+    print r.status_code
+    exit(1)
 
-result = json.loads(r.text)
-print json.dumps(result, indent=2, sort_keys=True)
+print r.text
+
+# result = json.loads(r.text)
+# print json.dumps(result, indent=2, sort_keys=True)
