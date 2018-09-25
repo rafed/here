711548e052547ac2ed2ca7d67c91ded7da182657
Rafed Yasir <rafed123@yahoo.com>
2018-09-25 15:52:05 +0600

    Server e dibo akhon

--- crawler.py
+++ crawler.py
@@ -15,13 +15,21 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 coordinates = [
-    ((28.614439,77.144623),(28.665286,77.260580))
+    ((28.747193,77.091064),(28.663211,77.1978375)),
+    ((28.663211,77.091064),(28.579229,77.1978375)),
+    ((28.579229,77.091064),(28.495247,77.1978375)),
+    ((28.747193,77.1978375),(28.663211,77.304611)),
+    ((28.663211,77.1978375),(28.579229,77.304611)),
+    ((28.579229,77.1978375),(28.495247,77.304611))
+    ## ((28.614439,77.144623),(28.665286,77.260580)) ## previous coord for test
 ]
 
 dt = datetime.now() - timedelta(minutes=30)
 date = dt.strftime("%d-%m-%Y")
 time = dt.strftime("%H:%M")
 weekday = dt.today().weekday()
+
+rows = []
 
 for pointX,pointY in coordinates:
     lat1,long1=pointX
@@ -90,8 +98,6 @@ for pointX,pointY in coordinates:
             jsonFile.write(r.text)
         exit(1)
 
-    rows = []
-
     try:
         for RWS in traffic_json['RWS']:
             for RW in RWS['RW']:
@@ -101,6 +107,7 @@ for pointX,pointY in coordinates:
                         src = RW['DE']
                         for FI in FIS['FI']:
                             dst = FI['TMC']['DE']
+                            LE = FI['TMC']['LE']
                             CN = FI['CF']['CN']
                             SP = FI['CF'][0]['SP']
                             SU = FI['CF'][0]['SU']
@@ -111,6 +118,7 @@ for pointX,pointY in coordinates:
                         dst = RW['DE']
                         for FI in FIS['FI']:
                             src = FI['TMC']['DE']
+                            LE = FI['TMC']['LE']
                             CN = FI['CF'][0]['CN']
                             SP = FI['CF'][0]['SP']
                             SU = FI['CF'][0]['SU']
@@ -118,11 +126,10 @@ for pointX,pointY in coordinates:
                             JF = FI['CF'][0]['JF']
                             SHP = FI['SHP'][0]['value'][0]
                     
-                    row =   (src, dst, CN, SP, SU, FF, JF, SHP, \
+                    row =   (src, dst, LE, CN, SP, SU, FF, JF, SHP, \
                             weekday, temperature, daylight, humidity, \
                             rainfall, rainDesc, windspeed, date, time, 0) # 0 for not holiday
                     rows.append(row)
-                    print row[0]
     except Exception as e:
         print "Field access error in traffic json!"
         with open("error.log.txt", "a") as error:
@@ -139,7 +146,7 @@ for pointX,pointY in coordinates:
         exit(1)
 
     try:
-        query = "insert into data values (%s" + ",%s"*17 + ")" ## %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+        query = "insert into data values (%s" + ",%s"*18 + ")" ## %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
         mycursor.executemany(query, rows)
         mydb.commit()
     except Exception as e:
