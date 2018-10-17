8ad145c48472148e2ff27182a232203aa24e3ab1
Rafed <rafed123@yahoo.com>
2018-10-18 02:49:22 +0600

    Data pre preprocessing

---
+++ categorical.py
@@ -0,0 +1,64 @@
+#!usr/bin/python
+
+# pip3 install mysql-connector
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="here",
+  passwd="here",
+  database="here"
+)
+
+mycursor = mydb.cursor()
+
+roads = set()
+
+sql = "SELECT DISTINCT src FROM data;"
+mycursor.execute(sql)
+result = mycursor.fetchall()
+
+for r in result:
+    roads.add(r[0])
+
+sql = "SELECT DISTINCT dst FROM data;"
+mycursor.execute(sql)
+result = mycursor.fetchall()
+
+for r in result:
+    roads.add(r[0])
+
+roads = list(roads)
+roads.sort()
+roads = dict(enumerate(roads))
+
+for r in roads: 
+    print(r, roads[r])
+
+with open("roads.txt", "w") as f:
+    for i in roads: 
+        s = ("%d %s\n") % (i, roads[i])
+        f.write(s)
+
+for i in roads: 
+    rd = roads[i]
+    
+    # sql = '''UPDATE data SET
+    #             src=CASE
+    #                 WHEN src='{0}' THEN '{1}'
+    #             END,
+    #             dst=CASE
+    #                 WHEN dst='{0}' THEN '{1}'
+    #             END
+    #             WHERE src='{0}' or dst='{0}' '''.format(rd, i)
+    
+    print("Running query {} for src for {}".format(i, rd))
+    sql = "UPDATE data SET src='{}' WHERE src='{}'".format(i, rd)
+    mycursor.execute(sql)
+    mydb.commit()
+
+    print("Running query {} for dst for {}".format(i, rd))
+    sql = "UPDATE data SET dst='{}' WHERE dst='{}'".format(i, rd)
+    mycursor.execute(sql)
+    mydb.commit()
--- preprocessing.sql
+++ preprocessing.sql
@@ -1,10 +1,19 @@
-UPDATE data SET holiday=1 WHERE weekday=6;
+-- please do this, data is getting huge :3 19 min 49.53 sec
+ALTER TABLE data DROP shp;
+DELETE FROM data WHERE LENGTH(dst)=0;
+
+-- fix dates
 UPDATE data SET weekday=MOD((weekday-1), 7) WHERE time="23:30" or time="23:45";
 UPDATE data set weekday=6 WHERE weekday=-1;
-DELETE FROM data WHERE LENGTH(dst)=0;
+
+-- not needed now since turning strings to numerals
 UPDATE data SET src=REPLACE(src, "\'", " ");
 UPDATE data SET dst=REPLACE(dst, "\'", " "); -- labh nai
 
-quotation space diye replace
+-- for dumping data
+select src,dst,cn,temperature,daylight,humidity,windspeed,time,holiday,jf from data into outfile '/tmp/lol.csv' fields terminated by ',' lines terminated by '\n';
+
+-- dump table
+mysqldump -uhere -phere here data > traffic.sql
 
 java -Xmx2000m -jar weka.jar
