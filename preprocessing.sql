e34158aac6469d6529b983a8d6d988448f236108
Rafed <rafed123@yahoo.com>
2019-01-09 01:06:06 +0600

    Machine details

--- preprocessing.sql
+++ preprocessing.sql
@@ -28,7 +28,7 @@ UPDATE data SET src=REPLACE(src, "\'", " ");
 UPDATE data SET dst=REPLACE(dst, "\'", " "); -- labh nai
 
 -- for dumping data
-select src,dst,weekday,temperature,daylight,humidity,windspeed,time,holiday,area,jf from data into outfile '/tmp/final.csv' fields terminated by ',' lines terminated by '\n';
+select time,src,dst,weekday,temperature,daylight,humidity,windspeed,rainDesc,holiday,area,jf from data into outfile '/tmp/traffic_full_preprocessed.csv' fields terminated by ',' lines terminated by '\n';
 
 -- useful stuff
 mysqldump -uhere -phere here data > traffic.sql
@@ -36,3 +36,43 @@ java -Xmx2000m -jar weka.jar
 
 
 ff,shp,le,sp,su,rainfall baad de
+
+
+########################################################
+currently rows 55,654,951
+per day around 700,000
+146010 delete where windspeed=*
+holiday update (19-10-2018, 07-11-2018, 21-11-2018, 23-11-2018)
+
+
+*
+Rain
+Drizzle
+Light rain
+
+Date range: 07-10-2018  26-12-2018
+
+Ubuntu 14.04.5 LTS
+Linux 4.15.0-20-generic x86_64
+
+CPU op-mode(s):        32-bit, 64-bit
+Byte Order:            Little Endian
+CPU(s):                24
+On-line CPU(s) list:   0-23
+Thread(s) per core:    2
+Core(s) per socket:    6
+Socket(s):             2
+NUMA node(s):          2
+Vendor ID:             GenuineIntel
+CPU family:            6
+Model:                 63
+Stepping:              2
+CPU MHz:               1200.068
+BogoMIPS:              4799.91
+Virtualization:        VT-x
+L1d cache:             32K
+L1i cache:             32K
+L2 cache:              256K
+L3 cache:              15360K
+NUMA node0 CPU(s):     0,2,4,6,8,10,12,14,16,18,20,22
+NUMA node1 CPU(s):     1,3,5,7,9,11,13,15,17,19,21,23
